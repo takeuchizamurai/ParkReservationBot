85b23900ad6a8b74ec1028370ebbffd71edd11b0
takeuchizamurai <vtms7446@yahoo.co.jp>
2026-04-09 21:24:28 +0900

    typo_fix

--- start.sh
+++ start.sh
@@ -3,9 +3,10 @@ set -e
 
 export DISPLAY=:99
 
-Xvfb :99 -screen - 1600x900x24 &
+Xvfb :99 -screen 0 1600x900x24 &
 fluxbox &
 x11vnc -display :99 -forever -shared -rfbport 5900 -nopw &
-websockify --web=/usr/share/novnc/ 6080 loaclhost:5900 &
+websockify --web=/usr/share/novnc/ 6080 localhost:5900 &
 
+sleep 2
 python -u main.py
